887a1f436eb1f4d4c912cc86f1ac25fe363c4f1e
Rohit Londhe <rohitlondhe446@gmail.com>
2022-12-25 13:18:55 +0530

    The Vite React and Tailwind frontend project setup template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  
+  return (
+    <div className="App">
+    <h1 className="text-6xl text-orange-600 bg-gray-300 h-screen flex items-center justify-center text-center">
+      Hello This is Vite + React + Tailwind 
+    </h1>
+    </div>
+  )
+}
+
+export default App
